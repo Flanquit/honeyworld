2ffd802a4be86e3c1000872e4d6f07c52b6b1a99
alpha <aflanquit@gmail.com>
2019-11-10 16:31:24 +0200

    without images onlt

--- products.php
+++ products.php
@@ -18,102 +18,68 @@
 
       <div class="container">
             <div class="row">
-               <div class="col-md-5 col-sm-5 col-12">
-                  <div class="section-heading">
-                 
-                     <h4 class="semi-bold"> Bottled Honey </h4>
+               <div class="col-md-4 col-sm-6 col-12">
+                  <div class="shop-1">
+                     <img src="img/content/shop/1.jpg" alt="img"> 
+                     <div class="shop-classic-sale-img shop-classic-hot-img">
+                        <h4>Sale</h4>
+                     </div>
+                     <h6>Bottled Honey </h6>
+                     <p><span>$200</span> $159</p>
                   </div>
-                  <div class="text-content mt-15">
-                     <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum</p>
-                  </div>
-              
-                  <!-- <a href="#" class="button-md button-primary mt-30">Read More</a> -->
                </div>
-               <div class="col-md-6 col-sm-6 col-12 offset-md-1"><img src="img/logos/who.jpg" class="position-relative extra-rounded-border shadow-primary z-index-9 mt-30-xs" alt="img"></div>
+               <div class="col-md-4 col-sm-6 col-12">
+                  <div class="shop-1">
+                     <img src="img/content/shop/2.jpg" alt="img"> 
+                     <h6>Bulk Honey </h6>
+                     <p><span>$200</span> $159</p>
+                  </div>
+               </div>
+               <div class="col-md-4 col-sm-6 col-12">
+                  <div class="shop-1">
+                     <img src="img/content/shop/3.jpg" alt="img"> 
+                     <div class="shop-classic-sale-img shop-classic-hot-img">
+                        <h4>Sale</h4>
+                     </div>
+                     <h6>Combed Honey</h6>
+                     <p><span>$200</span> $159</p>
+                  </div>
+               </div>
+               
             </div>
-
-
-
+            <!-- <div class="pagination mt-50"> <a href="#" class="active">1</a> </div> -->
             <div class="row">
-               <div class="col-md-5 col-sm-5 col-12">
-                  <div class="section-heading">
-                 
-                     <h4 class="semi-bold">Bulk Honey</h4>
+               <div class="col-md-4 col-sm-6 col-12">
+                  <div class="shop-1">
+                     <img src="img/content/shop/1.jpg" alt="img"> 
+                     <div class="shop-classic-sale-img shop-classic-hot-img">
+                        <h4>Sale</h4>
+                     </div>
+                     <h6>Honey Wax </h6>
+                     <p><span>$200</span> $159</p>
                   </div>
-                  <div class="text-content mt-15">
-                     <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
-                  </div>
-              
-                  <!-- <a href="#" class="button-md button-primary mt-30">Read More</a> -->
                </div>
-               <div class="col-md-6 col-sm-6 col-12 offset-md-1">
-                   <img src="img/logos/who.jpg" class="position-relative extra-rounded-border shadow-primary z-index-9 mt-30-xs" alt="img">
-                
-                </div>
-            </div>
-
-
-            <div class="row">
-               <div class="col-md-5 col-sm-5 col-12">
-                  <div class="section-heading">
-                 
-                     <h4 class="semi-bold">Combed Honey</h4>
+               <div class="col-md-4 col-sm-6 col-12">
+                  <div class="shop-1">
+                     <img src="img/content/shop/2.jpg" alt="img"> 
+                     <div class="shop-classic-sale-img shop-classic-hot-img">
+                        <h4>Coming Soon</h4>
+                     </div>
+                     <h6>Honey Skin Care</h6>
+                     <p><span>$200</span> $159</p>
                   </div>
-                  <div class="text-content mt-15">
-                     <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
-                  </div>
-              
-                  <!-- <a href="#" class="button-md button-primary mt-30">Read More</a> -->
                </div>
-               <div class="col-md-6 col-sm-6 col-12 offset-md-1"><img src="img/logos/who.jpg" class="position-relative extra-rounded-border shadow-primary z-index-9 mt-30-xs" alt="img"></div>
-            </div>
-
-
-            <div class="row">
-               <div class="col-md-5 col-sm-5 col-12">
-                  <div class="section-heading">
-                 
-                     <h4 class="semi-bold">Honey Wax </h4>
+               <div class="col-md-4 col-sm-6 col-12">
+                  <div class="shop-1">
+                     <img src="img/content/shop/2.jpg" alt="img"> 
+                     <div class="shop-classic-sale-img shop-classic-hot-img">
+                        <h4>Coming Soon</h4>
+                     </div>
+                     <h6>Honey Wine </h6>
+                     <p><span>$200</span> $159</p>
                   </div>
-                  <div class="text-content mt-15">
-                     <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
-                  </div>
-              
-                  <!-- <a href="#" class="button-md button-primary mt-30">Read More</a> -->
                </div>
-               <div class="col-md-6 col-sm-6 col-12 offset-md-1"><img src="img/logos/who.jpg" class="position-relative extra-rounded-border shadow-primary z-index-9 mt-30-xs" alt="img"></div>
-            </div>
-
-
-            <div class="row">
-               <div class="col-md-5 col-sm-5 col-12">
-                  <div class="section-heading">
-                 
-                     <h4 class="semi-bold">Honey Skin Care Products  </h4>
-                  </div>
-                  <div class="text-content mt-15">
-                     <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
-                  </div>
-              
-                  <!-- <a href="#" class="button-md button-primary mt-30">Read More</a> -->
-               </div>
-               <div class="col-md-6 col-sm-6 col-12 offset-md-1"><img src="img/logos/who.jpg" class="position-relative extra-rounded-border shadow-primary z-index-9 mt-30-xs" alt="img"></div>
-            </div>
-
-
-            <div class="row">
-               <div class="col-md-5 col-sm-5 col-12">
-                  <div class="section-heading">
-                 
-                     <h4 class="semi-bold">Honey Wine   </h4>
-                  </div>
-                  <div class="text-content mt-15">
-                     <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
-                  </div>
-              
-                  <!-- <a href="#" class="button-md button-primary mt-30">Read More</a> -->
-               </div>
-               <div class="col-md-6 col-sm-6 col-12 offset-md-1"><img src="img/logos/who.jpg" class="position-relative extra-rounded-border shadow-primary z-index-9 mt-30-xs" alt="img"></div>
+               
             </div>
          </div>
       </div>
